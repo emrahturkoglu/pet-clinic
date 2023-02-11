e647e8f3d19d264e210f2fadb9183bfaf5e3e4cd
EMRAH TURKOGLU <emrah.turkoglu@consultant.turkcell.com.tr>
2023-02-11 08:30:43 +0300

    Create Interfaces for Pet Clinic Services. Closes #5

---
+++ pet-clinic-data/src/main/java/com/emrah/petclinic/service/VetService.java
@@ -0,0 +1,11 @@
+package com.emrah.petclinic.service;
+
+import com.emrah.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
