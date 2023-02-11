27b18bc0598aacb66d8426e675621b1969073012
EMRAH TURKOGLU <emrah.turkoglu@consultant.turkcell.com.tr>
2023-02-11 08:37:45 +0300

    Adding BaseEntity Class. Closes #10

--- pet-clinic-data/src/main/java/com/emrah/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/emrah/petclinic/model/Pet.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
-public class Pet {
+public class Pet extends BaseEntity {
     private PetType petType;
     private Owner owner;
     private LocalDateTime birthDate;
