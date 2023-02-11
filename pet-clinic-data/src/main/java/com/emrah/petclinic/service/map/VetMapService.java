1c04250e66aef3d9735e9cbfb1c3be08db197849
EMRAH TURKOGLU <emrah.turkoglu@consultant.turkcell.com.tr>
2023-02-11 16:31:13 +0300

    Implement Map Based Services

---
+++ pet-clinic-data/src/main/java/com/emrah/petclinic/service/map/VetMapService.java
@@ -0,0 +1,35 @@
+package com.emrah.petclinic.service.map;
+
+import com.emrah.petclinic.model.Vet;
+import com.emrah.petclinic.service.VetService;
+
+import java.util.Set;
+
+public class VetMapService extends AbstractMapService<Vet, Long> implements VetService {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+}
