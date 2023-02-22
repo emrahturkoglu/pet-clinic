65ab17fee99d89575c64a0def3606aa2fcfbc43d
EMRAH TURKOGLU <emrah.turkoglu@consultant.turkcell.com.tr>
2023-02-22 16:10:18 +0300

    Added Owners List and Updated Controller. Closes #16

--- pet-clinic-web/src/main/java/com/emrah/petclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/emrah/petclinic/controller/OwnerController.java
@@ -1,15 +1,22 @@
 package com.emrah.petclinic.controller;
 
+import com.emrah.petclinic.service.OwnerService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/owners")
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
     @GetMapping({"", "/",  "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
