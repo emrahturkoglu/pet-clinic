c87540d955be24e9447d362307f1671d6bf0eba9
EMRAH TURKOGLU <emrah.turkoglu@consultant.turkcell.com.tr>
2023-02-22 16:14:49 +0300

    Added Vets List and Updated Controller. Closes #15

--- pet-clinic-web/src/main/java/com/emrah/petclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/emrah/petclinic/controller/VetController.java
@@ -1,15 +1,22 @@
 package com.emrah.petclinic.controller;
 
+import com.emrah.petclinic.service.VetService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/vets")
 public class VetController {
 
+    private final VetService vetService;
+
     @GetMapping({"", "/",  "/index", "/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
