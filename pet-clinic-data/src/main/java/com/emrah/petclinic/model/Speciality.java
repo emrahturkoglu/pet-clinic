071905226a070030614520cc0e05234af6f3a810
EMRAH TURKOGLU <emrah.turkoglu@consultant.turkcell.com.tr>
2023-02-25 21:30:56 +0300

    Convert Vets to JPA Entities. Closes #37

--- pet-clinic-data/src/main/java/com/emrah/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/emrah/petclinic/model/Speciality.java
@@ -3,8 +3,16 @@ package com.emrah.petclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
 @Getter
 @Setter
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
+
+    @Column(name = "description")
     private String description;
 }
