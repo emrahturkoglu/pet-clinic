dbfdbc40f523d5040868f11f97016d25fe6489e7
EMRAH TURKOGLU <emrah.turkoglu@consultant.turkcell.com.tr>
2023-02-25 21:56:09 +0300

    Creating Spring Data Jpa Service for Owners. Closes #40

--- pet-clinic-data/src/main/java/com/emrah/petclinic/repository/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/emrah/petclinic/repository/OwnerRepository.java
@@ -4,4 +4,5 @@ import com.emrah.petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findByLastName(String lastName);
 }
---
+++ pet-clinic-data/src/main/java/com/emrah/petclinic/service/jpa/OwnerJpaService.java
@@ -0,0 +1,51 @@
+package com.emrah.petclinic.service.jpa;
+
+import com.emrah.petclinic.model.Owner;
+import com.emrah.petclinic.repository.OwnerRepository;
+import com.emrah.petclinic.service.OwnerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+@Profile("springdatajpa")
+public class OwnerJpaService implements OwnerService {
+
+    private final OwnerRepository ownerRepository;
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> owners = new HashSet<>();
+        ownerRepository.findAll().forEach(owners::add);
+        return owners;
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return ownerRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return ownerRepository.save(owner);
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        ownerRepository.delete(owner);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        ownerRepository.deleteById(id);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName);
+    }
+}
