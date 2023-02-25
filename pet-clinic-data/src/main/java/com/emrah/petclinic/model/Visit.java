f679485b93f969dfcdb4ca2f0c630fc4b9dd5551
EMRAH TURKOGLU <emrah.turkoglu@consultant.turkcell.com.tr>
2023-02-25 21:35:21 +0300

    Creating Visit Entity. Closes #38

--- pet-clinic-data/src/main/java/com/emrah/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/emrah/petclinic/model/Visit.java
@@ -3,12 +3,21 @@ package com.emrah.petclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
 @Getter
 @Setter
+@Table(name = "visits")
 public class Visit extends BaseEntity {
-    private LocalDate localDate;
+    @Column(name = "date")
+    private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 }
