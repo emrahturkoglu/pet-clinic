98f2a46f31cdbe85022ada1893d894ad1e932817
EMRAH TURKOGLU <emrah.turkoglu@consultant.turkcell.com.tr>
2023-02-25 09:17:05 +0300

    Creating Specialities and Adding to Vets on Startup. Closes #34

--- pet-clinic-data/src/main/java/com/emrah/petclinic/service/map/VetMapService.java
+++ pet-clinic-data/src/main/java/com/emrah/petclinic/service/map/VetMapService.java
@@ -1,13 +1,19 @@
 package com.emrah.petclinic.service.map;
 
+import com.emrah.petclinic.model.Speciality;
 import com.emrah.petclinic.model.Vet;
+import com.emrah.petclinic.service.SpecialityService;
 import com.emrah.petclinic.service.VetService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@RequiredArgsConstructor
 public class VetMapService extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialityService specialityService;
 
     @Override
     public Set<Vet> findAll() {
@@ -21,6 +27,14 @@ public class VetMapService extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet object) {
+        if(object.getSpecialities().size() > 0) {
+            object.getSpecialities().forEach(speciality -> {
+                if(speciality.getId() == null) {
+                    Speciality savedSpeciality = specialityService.save(speciality);
+                    speciality.setId(savedSpeciality.getId());
+                }
+            });
+        }
         return super.save(object);
     }
 
--- pet-clinic-web/src/main/java/com/emrah/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/emrah/petclinic/bootstrap/DataLoader.java
@@ -1,11 +1,9 @@
 package com.emrah.petclinic.bootstrap;
 
-import com.emrah.petclinic.model.Owner;
-import com.emrah.petclinic.model.Pet;
-import com.emrah.petclinic.model.PetType;
-import com.emrah.petclinic.model.Vet;
+import com.emrah.petclinic.model.*;
 import com.emrah.petclinic.service.OwnerService;
 import com.emrah.petclinic.service.PetTypeService;
+import com.emrah.petclinic.service.SpecialityService;
 import com.emrah.petclinic.service.VetService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
@@ -20,9 +18,18 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final OwnerService ownerService;
     private final PetTypeService petTypeService;
+    private final SpecialityService specialityService;
 
     @Override
     public void run(String... args) {
+        int count = petTypeService.findAll().size();
+
+        if(count == 0){
+            loadData();
+        }
+    }
+
+    private void loadData() {
         PetType dog = new PetType();
         dog.setName("Dog");
         PetType savedDogPetType = petTypeService.save(dog);
@@ -30,6 +37,18 @@ public class DataLoader implements CommandLineRunner {
         PetType cat = new PetType();
         dog.setName("Cat");
         PetType savedCatPetType = petTypeService.save(cat);
+
+        Speciality radiology = new Speciality();
+        radiology.setDescription("Radiology");
+        Speciality savedRadiology = specialityService.save(radiology);
+
+        Speciality surgery = new Speciality();
+        surgery.setDescription("Surgery");
+        Speciality savedSurgery = specialityService.save(surgery);
+
+        Speciality dentistry = new Speciality();
+        dentistry.setDescription("Dentistry");
+        Speciality savedDentistry = specialityService.save(dentistry);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
@@ -68,12 +87,14 @@ public class DataLoader implements CommandLineRunner {
         Vet vet1 = new Vet();
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
+        vet1.getSpecialities().add(savedRadiology);
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
+        vet2.getSpecialities().add(savedSurgery);
 
         vetService.save(vet2);
 
