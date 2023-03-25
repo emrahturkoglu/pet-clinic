cc16d582635902fa87985e7c2bb36a76b7b02fbe
emrahturkoglu <emrahturkoglu89@gmail.com>
2023-03-25 20:39:42 +0300

    Implementing Find Owner. Closes #54

--- pet-clinic-data/src/main/java/com/emrah/petclinic/repository/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/emrah/petclinic/repository/OwnerRepository.java
@@ -3,6 +3,9 @@ package com.emrah.petclinic.repository;
 import com.emrah.petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
     Owner findByLastName(String lastName);
+    List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/com/emrah/petclinic/service/OwnerService.java
+++ pet-clinic-data/src/main/java/com/emrah/petclinic/service/OwnerService.java
@@ -2,6 +2,9 @@ package com.emrah.petclinic.service;
 
 import com.emrah.petclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
+    List<Owner> findAllByLastNameLike(String lastName);
 }
